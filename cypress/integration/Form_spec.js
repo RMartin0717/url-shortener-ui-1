60889fa671810ba174d8083a5372c38797984c6d
Riley Martin <riley.willow@gmail.com>
2021-06-15 11:40:43 -0600

    Adjust POST request stubbing

--- cypress/integration/Form_spec.js
+++ cypress/integration/Form_spec.js
@@ -3,6 +3,15 @@ describe('Form', () => {
     cy.fixture('urls').then((urls) => {
       cy.intercept('http://localhost:3001/api/v1/urls', { body: urls }).as('urls')
     })
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
+      statusCode: 201,
+      body: {
+        title: 'Test123',
+        long_url: 'https://somekindoflongurl.com/abcdefgTest123',
+        short_url: 'http://localhost:3001/useshorturl/99999',
+        id: 99999
+      }
+    })
     cy.visit('http://localhost:3000/')
   })
   it('Should display the Form with the proper inputs when a user visits the page', () => {
@@ -18,13 +27,9 @@ describe('Form', () => {
       .get('form input[name="urlToShorten"]').should('have.value', 'https://somekindoflongurl.com/abcdefg')
   })
   it('Should render the new shortened URL when a user fills out and submits the form', () => {
-    const uniqueUrl = Date.now()
-    cy.get('form input[name="title"]').type(`Test${uniqueUrl}`)
-      .get('form input[name="urlToShorten"]').type(`https://somekindoflongurl.com/abcdefg${uniqueUrl}`)
-      // .intercept('POST', 'http://localhost:3001/api/v1/urls', {
-      //   statusCode: 200
-      // })
+    cy.get('form input[name="title"]').type(`Test123`)
+      .get('form input[name="urlToShorten"]').type(`https://somekindoflongurl.com/abcdefgTest123`)
       .get('button').click()
-      .get('h3').should('have.text', `Test${uniqueUrl}`)
+      .get('h3').should('have.text', `Test123`)
   })
 })

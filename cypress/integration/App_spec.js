37a3ede725fe56db29ab9ecc5fc22b77676d6030
Riley Martin <riley.willow@gmail.com>
2021-06-15 10:16:02 -0600

    Set up cypress testing

---
+++ cypress/integration/App_spec.js
@@ -0,0 +1,8 @@
+describe('App', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+  it('Should display the page title and the existing shortened URLs when a user visits the page', () => {
+
+  })
+})
---
+++ cypress/integration/Form_spec.js
@@ -0,0 +1,11 @@
+describe('Form', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+  it('Should display the Form with the proper inputs when a user visits the page', () => {
+
+  })
+  it('Should reflect the information in the input fields when a user fills out the form', () => {
+
+  })
+})

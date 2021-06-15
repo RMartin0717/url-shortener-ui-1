54fbee550b7b62978c9c237df037e78518b91a60
Riley Martin <riley.willow@gmail.com>
2021-06-15 10:23:55 -0600

    Implement urls fixture as stub for intercepts

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -1,5 +1,8 @@
 describe('App', () => {
   beforeEach(() => {
+    cy.fixture('urls').then((urls) => {
+      cy.intercept('http://localhost:3001/api/v1/urls', { body: urls }).as('urls')
+    })
     cy.visit('http://localhost:3000/')
   })
   it('Should display the page title and the existing shortened URLs when a user visits the page', () => {

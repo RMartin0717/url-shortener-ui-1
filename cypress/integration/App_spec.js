76f8c591d4ab1b5198287b311b4b81b1de845c9d
Riley Martin <riley.willow@gmail.com>
2021-06-15 10:34:34 -0600

    Write tests for form inputs

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -5,7 +5,12 @@ describe('App', () => {
     })
     cy.visit('http://localhost:3000/')
   })
-  it('Should display the page title and the existing shortened URLs when a user visits the page', () => {
-
+  it('Should display the page title when a user visits the page', () => {
+    cy.get('h1').should('have.text', 'URL Shortener')
+  })
+  it('Should display the existing shortened URLs when a user visits the page', () => {
+    cy.get('h3').should('have.text', 'Awesome photo')
+      .get('a').should('have.text', 'http://localhost:3001/useshorturl/1')
+      .get('p').should('have.text', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
   })
 })
--- cypress/integration/Form_spec.js
+++ cypress/integration/Form_spec.js
@@ -6,10 +6,16 @@ describe('Form', () => {
     cy.visit('http://localhost:3000/')
   })
   it('Should display the Form with the proper inputs when a user visits the page', () => {
-
+    cy.get('form input[name="title"]').should('exist')
+      .get('form input[name="urlToShorten"]').should('exist')
   })
-  it('Should reflect the information in the input fields when a user fills out the form', () => {
-
+  it('Should reflect the information in the title input field when a user fills out the form', () => {
+    cy.get('form input[name="title"]').type('Test4')
+      .get('form input[name="title"]').should('have.value', 'Test4')
+  })
+  it('Should reflect the information in the urlToShorten input field when a user fills out the form', () => {
+    cy.get('form input[name="urlToShorten"]').type('https://somekindoflongurl.com/abcdefg')
+      .get('form input[name="urlToShorten"]').should('have.value', 'https://somekindoflongurl.com/abcdefg')
   })
   it('Should render the new shortened URL when a user fills out and submits the form', () => {
 

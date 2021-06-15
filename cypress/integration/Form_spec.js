6a67c54c1774ebd22220b6b2f3060a453695f981
Riley Martin <riley.willow@gmail.com>
2021-06-15 10:21:13 -0600

    Create urls fixture

--- cypress/integration/Form_spec.js
+++ cypress/integration/Form_spec.js
@@ -1,11 +1,17 @@
 describe('Form', () => {
   beforeEach(() => {
+    cy.fixture('urls').then((urls) => {
+      cy.intercept('http://localhost:3001/api/v1/urls', { body: urls }).as('urls')
+    })
     cy.visit('http://localhost:3000/')
   })
   it('Should display the Form with the proper inputs when a user visits the page', () => {
 
   })
   it('Should reflect the information in the input fields when a user fills out the form', () => {
+
+  })
+  it('Should render the new shortened URL when a user fills out and submits the form', () => {
 
   })
 })
